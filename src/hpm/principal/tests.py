75cfce99fdbe298fbf9352faa3f366a6dd224f90
Santiago <santiago.kenshinvaldez@gmail.com>
2014-04-12 05:01:13 -0400

    Cambios en template, y test

--- src/hpm/principal/tests.py
+++ src/hpm/principal/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from principal.models import Usuario
 
 # Create your tests here.
+
+
+class UsuarioTestCase(TestCase):
+    def setUp(self):
+        Usuario.objects.create(username="usera", nombre="A", apellido = "apellido", telefono = "telefono", password = "password", email = "email", ci = 0)
+        Usuario.objects.create(username="userb", nombre="B", apellido = "apellido", telefono = "telefono", password = "password", email = "email", ci = 0)
+
+    def test_nombre_correcto(self):
+        """Test si se guardan correctamente los objetos"""
+        a = Usuario.objects.get(username="usera")
+        b = Usuario.objects.get(username="userb")
+        self.assertEqual(a.nombre, 'A')
+        self.assertEqual(b.nombre, 'B')
